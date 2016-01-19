592c528ef7d448199c0bcaf65b354c724e0be529
Vlad <slayerru@yandex.ru>
2016-01-19 23:42:09 +0300

    added recyclerview for categorymembers

--- app/src/main/java/com/randomname/mrakopedia/ui/categorymembers/CategoryMembersFragment.java
+++ app/src/main/java/com/randomname/mrakopedia/ui/categorymembers/CategoryMembersFragment.java
@@ -3,16 +3,25 @@ package com.randomname.mrakopedia.ui.categorymembers;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.randomname.mrakopedia.R;
 import com.randomname.mrakopedia.api.MrakopediaApiWorker;
 import com.randomname.mrakopedia.models.api.categorymembers.CategoryMembersResult;
+import com.randomname.mrakopedia.models.api.categorymembers.Categorymembers;
 import com.randomname.mrakopedia.ui.RxBaseFragment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import rx.Subscriber;
 import rx.Subscription;
@@ -25,12 +34,24 @@ import rx.schedulers.Schedulers;
 public class CategoryMembersFragment extends RxBaseFragment {
     private static final String TAG = "categoryMembersFragment";
 
+    @Bind(R.id.category_members_recycler_view)
+    RecyclerView recyclerView;
+
+    CategoryMembersAdapter adapter;
+    ArrayList<Categorymembers> categorymembersArrayList;
+
     public CategoryMembersFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.category_members_fragment, null);
         ButterKnife.bind(this, view);
+
+        categorymembersArrayList = new ArrayList<>();
+        adapter = new CategoryMembersAdapter(categorymembersArrayList);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         Subscription getCategoryMembersSubscription =
                 MrakopediaApiWorker
@@ -51,10 +72,48 @@ public class CategoryMembersFragment extends RxBaseFragment {
 
                             @Override
                             public void onNext(CategoryMembersResult categoryMembersResult) {
+                                categorymembersArrayList
+                                        .addAll(Arrays.asList(categoryMembersResult.getQuery().getCategorymembers()));
+                                adapter.notifyDataSetChanged();
                             }
                         });
         bindToLifecycle(getCategoryMembersSubscription);
 
         return view;
+    }
+
+    private class CategoryMembersAdapter extends RecyclerView.Adapter<CategoryMembersAdapter.ViewHolder> {
+
+        ArrayList<Categorymembers> categorymembersArrayList;
+
+        public CategoryMembersAdapter(ArrayList<Categorymembers> categorymembers) {
+            categorymembersArrayList = categorymembers;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_member_view_holder, null);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.titleTextView.setText(categorymembersArrayList.get(position).getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return categorymembersArrayList.size();
+        }
+
+        protected class ViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView titleTextView;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                titleTextView = (TextView) itemView.findViewById(R.id.title_text_view);
+            }
+        }
     }
 }
