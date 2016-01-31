055cbc71a1451be491c2b29cf8ca886db55fce14
Vlad <slayerru@yandex.ru>
2016-01-31 23:33:12 +0300

    add crashlitic

--- app/src/main/java/com/randomname/mrakopedia/MrakopediaApplication.java
+++ app/src/main/java/com/randomname/mrakopedia/MrakopediaApplication.java
@@ -2,10 +2,12 @@ package com.randomname.mrakopedia;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
+import io.fabric.sdk.android.Fabric;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 import io.realm.rx.RealmObservableFactory;
@@ -17,6 +19,7 @@ public class MrakopediaApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         RealmConfiguration config = new RealmConfiguration.Builder(this).rxFactory(new RealmObservableFactory()).build();
         Realm.setDefaultConfiguration(config);
 
